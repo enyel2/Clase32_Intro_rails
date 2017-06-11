fcdf4fcb6507ffcd9db97659036c72699921517c
Enyel <enyeldoc2011@gmail.com>
2017-06-11 01:25:16 -0400

    segundo

--- app/controllers/landings_controller.rb
+++ app/controllers/landings_controller.rb
@@ -6,5 +6,14 @@ class LandingsController < ApplicationController
   end
 
   def z
+  	#siempre variables de instancias
+  	#@val = 5
+  	#@val = [1,2,3,4,5,6,7]
+  	@val = []
+  	20.times do |i|
+  		@val << "nombre#{i+1}"
+  	end
+  	#@valor = "Esta es mi variable del metodo del controller"
+
   end
 end
